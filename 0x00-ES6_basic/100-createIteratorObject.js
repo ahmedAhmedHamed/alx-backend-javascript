bebf3c0c7b887772f838348f0172b2c75327bed8
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-09-30 20:16:10 +0300

    (100) eslint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,9 @@
-export default function *createIteratorObject(report) {
-  for (const key in report['allEmployees']) {
-    for (const name of report['allEmployees'][key]) {
-      yield name;
+export default function* createIteratorObject(report) {
+  for (const key in report.allEmployees) {
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, key)) {
+      for (const name of report.allEmployees[key]) {
+        yield name;
+      }
     }
   }
 }
