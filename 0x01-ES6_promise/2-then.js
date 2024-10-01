2749d42c3839087ac9d4eee35557aa77f94a3215
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-01 18:43:52 +0300

    (2) using .then instead of finally

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,4 +1,4 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => ({ status: 200, body: 'success' }),
-    () => (new Error())).finally(() => console.log('Got a response from the API'));
+    () => (new Error())).then(() => console.log('Got a response from the API'));
 }
