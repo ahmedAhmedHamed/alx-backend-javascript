8361d4171d2b9ee57f015f3735a92841a290b801
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-01 18:38:03 +0300

    (2) eslint

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,4 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    return {status:200, body: 'success'};
-  }, () => {
-    return new Error();
-  }).finally(() => console.log("Got a response from the API"));
-
+  promise.then(() => ({ status: 200, body: 'success' }),
+    () => (new Error())).finally(() => console.log('Got a response from the API'));
 }
