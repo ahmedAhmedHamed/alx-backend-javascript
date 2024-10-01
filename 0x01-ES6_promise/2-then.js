912d99b4e5ddc895ca7f58a04ab8367f7cde1965
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-01 18:48:24 +0300

    (2) using .finally at the end

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
   promise.then(() => ({ status: 200, body: 'success' })).catch(
     () => (new Error()),
-  ).then(() => console.log('Got a response from the API'));
+  ).finally(() => console.log('Got a response from the API'));
 }
