100a1b024f18581b8d6caaa62d23e084f5450a09
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-02 13:34:28 +0300

    (6) using allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,7 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => (result.map((r) => ({ 'status': r.status,
-      value: r.status === 'fulfilled' ? r.value : String(r.reason)}))));
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => (result.map((r) => ({
+      status: r.status,
+      value: r.status === 'fulfilled' ? r.value : String(r.reason),
+    }))));
 }
