3faff67cade04c537d372f28be3995f29770c561
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-02 13:11:48 +0300

    (6) separated the two results

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,6 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => ({ status: 'resolved', value: result }))
-    .catch((error) => ({ status: 'rejected', value: error }));
+    .then((result) => ([{ status: 'resolved', value: result[0] },
+      { status: 'resolved', value: result[1] }]))
+    .catch((error) => ([{ status: 'rejected', value: error }]));
 }
