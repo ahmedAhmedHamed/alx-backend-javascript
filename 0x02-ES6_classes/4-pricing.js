058b20db2d4e10f28febf5975f569993f7ee4b6a
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-03 17:05:57 +0300

    (4) added static method

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -20,6 +20,10 @@ export default class Pricing {
     this._currency = currency;
   }
 
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
