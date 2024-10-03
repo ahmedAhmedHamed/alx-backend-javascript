66d4902a4c39dba93c75bd6518d386a2ec1d11e0
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-03 16:53:37 +0300

    (2) eslint

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof(name) !== "string") { throw new TypeError(`TypeError: Name must be a string`); }
-    if (typeof(length) !== "number") { throw new TypeError(`TypeError: Length must be a number`); }
-    if (typeof(students) !== "object") { throw new TypeError(`TypeError: Students must be a object`); }
+    if (typeof (name) !== 'string') { throw new TypeError('TypeError: Name must be a string'); }
+    if (typeof (length) !== 'number') { throw new TypeError('TypeError: Length must be a number'); }
+    if (typeof (students) !== 'object') { throw new TypeError('TypeError: Students must be a object'); }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -13,7 +13,7 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof(name) !== "string") { throw new TypeError(`TypeError: Name must be a string`); }
+    if (typeof (name) !== 'string') { throw new TypeError('TypeError: Name must be a string'); }
     this._name = name;
   }
 
@@ -22,7 +22,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof(length) !== "number") { throw new TypeError(`TypeError: Length must be a number`); }
+    if (typeof (length) !== 'number') { throw new TypeError('TypeError: Length must be a number'); }
     this._length = length;
   }
 
@@ -31,7 +31,7 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (typeof(students) !== "object") { throw new TypeError(`TypeError: Students must be a object`); }
+    if (typeof (students) !== 'object') { throw new TypeError('TypeError: Students must be a object'); }
     this._students = students;
   }
 }
