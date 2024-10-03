0af5a1dd67ff2365f6b4c2ac366e7d9f4143aa58
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-03 16:49:43 +0300

    (2) checking before setting

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof(name) !== "string") { throw new TypeError(`TypeError: Name must be a string`); }
+    if (typeof(length) !== "number") { throw new TypeError(`TypeError: Length must be a number`); }
+    if (typeof(students) !== "object") { throw new TypeError(`TypeError: Students must be a object`); }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -10,6 +13,7 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
+    if (typeof(name) !== "string") { throw new TypeError(`TypeError: Name must be a string`); }
     this._name = name;
   }
 
@@ -18,6 +22,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
+    if (typeof(length) !== "number") { throw new TypeError(`TypeError: Length must be a number`); }
     this._length = length;
   }
 
@@ -26,6 +31,7 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
+    if (typeof(students) !== "object") { throw new TypeError(`TypeError: Students must be a object`); }
     this._students = students;
   }
 }
