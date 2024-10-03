b3e745342252c0289a8d22479b1ea23a12272e88
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-03 17:04:16 +0300

    (4)

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+export default class Pricing {
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+}
