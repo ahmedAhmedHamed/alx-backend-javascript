5268396a6038c568311496b6bfbd499402e90535
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-08 18:11:39 +0300

    (10) fixed throwing

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) { throw new Error('Cannot Process'); }
   map.forEach((value, key) => {
-    if (typeof (value) === 'number') { throw new Error('Cannot Process'); }
+    if (typeof (value) !== 'number') { throw new Error('Cannot Process'); }
     if (value === 1) { map.set(key, 100); }
   });
   return map;
