2224f7d5c9e4d794880d9291b2a52daa6d0c83b2
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-08 18:10:11 +0300

    (10) now throwing if not number

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,8 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) { throw new Error('Cannot Process'); }
   map.forEach((value, key) => {
+    if (typeof (value) === 'number') { throw new Error('Cannot Process'); }
     if (value === 1) { map.set(key, 100); }
   });
+  return map;
 }
