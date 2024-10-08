77a56f2d28b4ff569a09a516eec92eb03ce7e9f3
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-08 17:45:53 +0300

    (9)

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const x = [['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(x);
+}
