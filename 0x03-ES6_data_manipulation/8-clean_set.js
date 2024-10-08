7eb7463d709708694a25ee2d396170efea983224
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-08 17:06:06 +0300

    (8) configured for test cases

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,10 @@ export default function cleanSet(set, startString) {
   let ret = '';
   if (startString === '') { return ret; }
   set.forEach((item) => {
-    if (item.startsWith(startString)) { ret = `${ret}${item.slice(startString.length)}-`; }
+    if (item.startsWith(startString)) {
+      const appended = item.length > startString.length ? `${item.slice(startString.length)}-` : '';
+      ret = `${ret}${appended}`;
+    }
   });
   return ret.slice(0, ret.length - 1);
 }
