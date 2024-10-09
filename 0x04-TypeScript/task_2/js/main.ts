78343d13a0caa0c26b52f7a7b9607148251468aa
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-09 17:50:04 +0300

    (7)

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -55,3 +55,10 @@ export function executeWork(employee: Director | Teacher) : string {
     if (isDirector(employee)) { return employee.workDirectorTasks(); }
     else { return employee.workTeacherTasks(); }
 }
+
+type Subjects = "Math" | "History"
+
+export function teachClass(todayClass: Subjects) {
+    return todayClass === "Math" ? "Teaching Math" : "Teaching History";
+}
+
