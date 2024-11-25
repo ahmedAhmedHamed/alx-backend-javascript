16d53e79b70eb97072166ac25796221a66b9eb09
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-25 18:25:58 +0200

    (2)

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    let numberOfStudents = 0;
+    const studentDistribution = {
+      CS: [0, []],
+      SWE: [0, []],
+    };
+    const lines = data.split('\n');
+    delete lines[0];
+    lines.forEach((line) => {
+      const fields = line.split(',');
+      if (fields.length === 4) {
+        numberOfStudents += 1;
+        studentDistribution[fields[3]][0] += 1;
+        studentDistribution[fields[3]][1].push(fields[0]);
+      }
+    });
+    console.log(`Number of students: ${numberOfStudents}`);
+    for (const [key, value] of Object.entries(studentDistribution)) {
+      process.stdout.write(`Number of students in ${key}: ${value[0]}. List: `);
+      for (let i = 0; i < value[0]; i += 1) {
+        process.stdout.write(`${value[1][i]}`);
+        if (i !== value[0] - 1) {
+          process.stdout.write(',');
+        } else {
+          console.log();
+        }
+      }
+    }
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+countStudents('./database.csv');
