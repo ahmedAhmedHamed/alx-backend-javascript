dda7bc7533499356704d34e4dd725dfc46b08ffe
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-11-25 19:49:11 +0200

    (4) exporting app

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
+const app = http.createServer((req, res) => {
   res.write('Hello Holberton School!');
   res.end();
 });
-server.listen(1245);
+app.listen(1245);
+
+module.exports = app;
