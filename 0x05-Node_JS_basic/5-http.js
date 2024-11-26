74373c2e707e8959659c40c54a21db0b9310fad0
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-11-26 16:23:02 +0200

    (5)

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,12 +1,63 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  if (req.url.endswith('students')) {
-    res.write('This is the list of our students\n');
 
+path = process.argv[2] || '';
+
+
+const fs = require('fs');
+
+function countStudents(file) {
+  return new Promise((resolve) => {
+    if (!fs.existsSync(path)) {
+      throw new Error('Cannot load the database');
+    }
+    if (!fs.statSync(path).isFile()) {
+      throw new Error('Cannot load the database');
+    }
+    fs.readFile(path, 'utf-8', (err, data) => {
+      const lines = data.trim()
+          .split('\n');
+      const studentDistribution = {};
+      const fieldNames = lines[0].split(',');
+      const studentPropertyNames = fieldNames.slice(0, fieldNames.length - 1);
+
+      for (const line of lines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+        const field = studentRecord[studentRecord.length - 1];
+        if (!Object.keys(studentDistribution).includes(field)) {
+          studentDistribution[field] = [];
+        }
+        const studentEntries = studentPropertyNames
+            .map((propName, idx) => [propName, studentPropValues[idx]]);
+        studentDistribution[field].push(Object.fromEntries(studentEntries));
+      }
+
+      const totalStudents = Object
+          .values(studentDistribution)
+          .reduce((pre, cur) => (pre || []).length + cur.length);
+
+      file.write(`Number of students: ${totalStudents}`);
+
+      for (const [field, specialization] of Object.entries(studentDistribution)) {
+        const studentNames = specialization.map((student) => student.firstname).join(', ');
+        file.write(`Number of students in ${field}: ${specialization.length}. List: ${studentNames}`);
+      }
+      resolve();
+    });
+  });
+}
+
+
+const app = http.createServer((req, res) => {
+  console.log('before');
+  if (req.url.endsWith('students')) {
+    countStudents(res).then(()=>res.end());
+  } else {
+    res.write('Hello Holberton School!');
+    res.end();
   }
-  res.write('Hello Holberton School!');
-  res.end();
+  console.log('after');
 });
 app.listen(1245);
 
