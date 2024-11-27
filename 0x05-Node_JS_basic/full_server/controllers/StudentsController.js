6adfd8b5fd829900abe6a59f357b9fe25afe272e
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-11-27 03:45:30 +0200

    (8)

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,45 @@
+import readDatabase from "../utils";
+
+class StudentsController {
+  static getAllStudents(request, response) {
+    const path = process.argv[2];
+    readDatabase(path).then((data) => {
+      response.status(200);
+      let sent = ['This is the list of our students'];
+      for (const key of Object.keys(data)) {
+        sent.push(`Number of students in ${key}: ${data[key].length}. ` +
+        `List: ${data[key].join(', ')}`);
+      }
+      response.send(sent.join('\n'));
+    }).catch((err) => {
+      response.status(500);
+      const message = err instanceof Error ? err.message : err.toString();
+      response.send(message);
+    });
+  }
+  static getAllStudentsByMajor(request, response) {
+    const queriedField = request.params['major'];
+    console.log(queriedField);
+    if (!(['SWE', 'CS'].includes(queriedField))) {
+      response.status(500);
+      response.send('Major parameter must be CS or SWE');
+      return;
+    }
+    const path = process.argv[2];
+    readDatabase(path).then((data) => {
+      response.status(200);
+      const sent = []
+      for (const key of Object.keys(data)) {
+        if (key === queriedField) { sent.push(`List: ${data[key].join(', ')}`); }
+      }
+      response.send(sent.join('\n'));
+    }).catch((err) => {
+      response.status(500);
+      const message = err instanceof Error ? err.message : err.toString();
+      response.send(message);
+    });
+  }
+}
+
+export default StudentsController;
+module.exports = StudentsController;
---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,44 @@
+import AppController from "./controllers/AppController";
+
+const fs = require('fs');
+
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(path)) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (!fs.statSync(path).isFile()) {
+      reject(new Error('Cannot load the database'));
+    }
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      const lines = data.trim().split('\n');
+      const studentDistribution = {};
+      const fieldNames = lines[0].split(',');
+      const studentPropertyNames = fieldNames.slice(0, fieldNames.length - 1);
+
+      for (const line of lines.slice(1)) {
+        const studentRecord = line.split(',');
+        const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+        const field = studentRecord[studentRecord.length - 1];
+        if (!Object.keys(studentDistribution).includes(field)) {
+          studentDistribution[field] = [];
+        }
+        const studentEntries = studentPropertyNames
+            .map((propName, idx) => [propName, studentPropValues[idx]]);
+        studentDistribution[field].push(Object.fromEntries(studentEntries));
+      }
+
+      for (const key of Object.keys(studentDistribution)) {
+        for (let i = 0; i < studentDistribution[key].length; i++) {
+          studentDistribution[key][i] = studentDistribution[key][i].firstname;
+        }
+      }
+      resolve(studentDistribution);
+    });
+  });
+}
+export default readDatabase;
+module.exports = readDatabase;
