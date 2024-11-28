ed4afc9afcb1c07046590b16a9aac695fb9c4dd0
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-11-29 01:27:01 +0200

    split tests (0)

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,12 +2,16 @@ const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
 
 describe('0-calcul', function () {
-  describe('#calculateNumber()', function () {
-    it('should return addition of both after rounding', function () {
-      assert.equal(calculateNumber(1, 3), 4);
-      assert.equal(calculateNumber(1, 3.7), 5);
-      assert.equal(calculateNumber(1.2, 3.7), 5);
-      assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
+  it('should return addition of both after rounding', function () {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return addition of both after rounding', function () {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should return addition of both after rounding', function () {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return addition of both after rounding', function () {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
