7b8c6043e5e4a8b52d8f3f54c03a33a62ee6e115
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-11-29 01:33:58 +0200

    more tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,16 +2,23 @@ const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
 
 describe('calculateNumber', function () {
-  it('should return addition of both after rounding', function () {
+  it('numbers', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it('should return addition of both after rounding', function () {
+  it('flaoting numbers', function () {
+    assert.strictEqual(calculateNumber(1.0, 3.0), 4);
+  });
+  it('round down', function () {
+    assert.strictEqual(calculateNumber(1.2, 3.2), 4);
+  });
+  it('round up', function () {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-  it('should return addition of both after rounding', function () {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  it('round down trailing 9s', function () {
+    assert.strictEqual(calculateNumber(1.499999, 3.499999), 4);
   });
-  it('should return addition of both after rounding', function () {
+  it('round up both', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+
 });
