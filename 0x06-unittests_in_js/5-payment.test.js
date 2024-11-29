64a5fbdab9152950c50ac25b4cab130d37937a55
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-11-29 02:54:41 +0200

    5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,37 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+
+  let mySpy;
+
+  beforeEach(() => {
+    if (!mySpy) {
+      mySpy = sinon.spy(console);
+    }
+  });
+
+  afterEach(() => {
+    mySpy.log.resetHistory();
+  });
+
+  it('tests sendPaymentRequestToApi.', () => {
+    const mySpy = sinon.spy(console);
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(mySpy.log.calledWith('The total is: 120')).to.be.true;
+    expect(mySpy.log.callCount).to.be.equal(1);
+  });
+
+  it('tests sendPaymentRequestToApi.', () => {
+    const mySpy = sinon.spy(console);
+
+    sendPaymentRequestToApi(10, 10);
+
+    expect(mySpy.log.calledWith('The total is: 20')).to.be.true;
+    expect(mySpy.log.callCount).to.be.equal(1);
+  });
+});
